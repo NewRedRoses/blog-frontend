a9367dd960789e156e542d46d7bd5766d00a556c
Mark A. <mark001616@gmail.com>
2024-11-24 11:36:49 -0500

    Add various initial styles

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./Styles/index.css";
 function Index() {
   const [posts, setPosts] = useState([]);
 
@@ -10,12 +11,15 @@ function Index() {
   }, []);
 
   return (
-    <div>
+    <div className="content">
       <h1>Welcome to Mark's blog</h1>
-      <ul>
+      <ul className="posts-list-container">
         {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={"/posts/" + post.id}>{post.title}</Link>
+          <li key={post.id} className="posts-list-item">
+            <span className="posts-list-item-date">2024-12-3</span>
+            <Link to={"/posts/" + post.id} className="posts-list-item-link">
+              {post.title}
+            </Link>
           </li>
         ))}
       </ul>
