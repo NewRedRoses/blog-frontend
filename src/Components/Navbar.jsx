b192816140a7f08f279339e9a34f7b2f4788072a
Mark A. <mark001616@gmail.com>
2024-11-24 11:46:39 -0500

    Add origami icon to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { Origami } from "lucide-react";
 import "../Styles/navbar.css";
 export default function Navbar() {
   return (
     <div className="content navbar-container">
       <div className="navbar-left">
-        <a href="/">Home</a>
+        <a href="/">
+          <Origami />
+        </a>
       </div>
       <div className="navbar-right">
         <a href="/">About</a>
