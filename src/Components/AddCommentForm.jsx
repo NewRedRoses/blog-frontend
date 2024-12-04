1e6ae5a244f5961ad1d2f5662ebe7b39ba634943
Mark A. <mark001616@gmail.com>
2024-12-03 22:14:36 -0500

    Add comment form and basic styling

---
+++ src/Components/AddCommentForm.jsx
@@ -0,0 +1,16 @@
+export default function AddCommentForm({ url }) {
+  return (
+    <form action={url} method="POST" className="add-comment-form">
+      <label htmlFor="username">Your Name</label>
+      <input type="text" name="username" required />
+
+      <label htmlFor="message">Your Message:</label>
+      <textarea name="message" id="message" required>
+        test
+      </textarea>
+      <button type="submit" className="add-comment-btn">
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,4 +1,6 @@
+import AddCommentForm from "./AddCommentForm";
 import Comment from "./Comment";
+import "../Styles/comments.css";
 
 export default function Comments() {
   const comments = [
@@ -6,23 +8,29 @@ export default function Comments() {
     { msg: "sample test", user: "sample guy" },
     { msg: "reddit moment", user: "annoying guy" },
     { msg: "Skibidi toilet", user: "skibidi toilet" },
+    {
+      msg: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque nemo assumenda distinctio. Hic inventore et repellendus pariatur sit itaque eum nemo laboriosam consequuntur a modi cum, voluptatem similique ipsam cupiditate?",
+      user: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, amet?",
+    },
   ];
   const mockDate = new Date();
   return (
     <>
       <h2>Comments</h2>
-      <p>these are where the comments will be held</p>
-      {comments.map((comment, index) => {
-        return (
-          <li key={index}>
-            <Comment
-              author={comment.user}
-              text={comment.msg}
-              date={mockDate.toDateString()}
-            />
-          </li>
-        );
-      })}
+      <AddCommentForm />
+      <ul className="list-of-comments">
+        {comments.map((comment, index) => {
+          return (
+            <li key={index}>
+              <Comment
+                author={comment.user}
+                text={comment.msg}
+                date={mockDate.toDateString()}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
