d7e7a01f2555cf0fcd22940e508be2ac0cf0fd1f
Mark A. <mark001616@gmail.com>
2024-12-04 23:46:58 -0500

    Render html content from db to react

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Comments from "./Comments";
+import parse from "html-react-parser";
+
 export default function Post() {
   const { postId } = useParams();
-  const [post, setPost] = useState([]);
-
+  const [post, setPost] = useState({ title: "", content: "" });
   useEffect(() => {
     fetch(`http://localhost:3000/posts/${postId}`)
       .then((response) => response.json())
@@ -14,7 +15,8 @@ export default function Post() {
   return (
     <div className="content">
       <h1 className="post-title">{post.title}</h1>
-      <div className="post-body">{post.content}</div>
+      <div className="post-body">{parse(post.content)}</div>
+
       <div className="comments-container">
         <Comments postId={postId} />
       </div>
