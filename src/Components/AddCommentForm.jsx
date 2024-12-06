c444b90e585f6f3ade3f287bf56f98af79832d63
Mark A. <mark001616@gmail.com>
2024-12-05 23:56:59 -0500

    Wont redirect to post api url endpoint and instead refresh page

--- src/Components/AddCommentForm.jsx
+++ src/Components/AddCommentForm.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
 export default function AddCommentForm({ url }) {
-  return (
-    <form action={url} method="POST" className="add-comment-form">
-      <label htmlFor="username">Your Name:</label>
-      <input type="text" name="username" required />
+  const [form, setForm] = useState({ username: "", message: "" });
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    options.body = JSON.stringify(form);
+    // Submit POST request
+    await fetch(url, options);
+    // Refresh (ideally use states but that will be for ... later)
+    window.location.reload(false);
+  }
+  return (
+    <form
+      action={url}
+      method="POST"
+      className="add-comment-form"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="username">Your Name:</label>
+      <input
+        type="text"
+        name="username"
+        value={form.username}
+        onChange={(e) => setForm({ ...form, username: e.target.value })}
+        required
+      />
+      {console.log}
       <label htmlFor="message">Your Message:</label>
-      <textarea name="message" id="message" required></textarea>
+      <textarea
+        name="message"
+        id="message"
+        value={form.message}
+        onChange={(e) => setForm({ ...form, message: e.target.value })}
+        required
+      ></textarea>
       <button type="submit" className="add-comment-btn">
         Submit
       </button>
