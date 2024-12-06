b340c0cc701adc718884f8abc8693fbee8878474
Mark A. <mark001616@gmail.com>
2024-12-05 21:45:30 -0500

    Add author and publish date details on pages

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -2,10 +2,17 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Comments from "./Comments";
 import parse from "html-react-parser";
+import { Shell } from "lucide-react";
+import { format } from "date-fns";
 
 export default function Post() {
   const { postId } = useParams();
-  const [post, setPost] = useState({ title: "", content: "" });
+  const [post, setPost] = useState({
+    title: "loading...",
+    content: "",
+    user: { name: "" },
+    date_published: new Date(),
+  });
   useEffect(() => {
     fetch(`http://localhost:3000/posts/${postId}`)
       .then((response) => response.json())
@@ -14,7 +21,18 @@ export default function Post() {
 
   return (
     <div className="content">
-      <h1 className="post-title">{post.title}</h1>
+      <div className="post-header">
+        <h1 className="post-title">{post.title}</h1>
+        <div className="post-details">
+          <div className="post-author">
+            <Shell className="profile-picture" />
+            {post.user.name}
+          </div>
+          <div className="post-date-published">
+            {format(post.date_published, "PP (p)")}
+          </div>
+        </div>
+      </div>
       <div className="post-body">{parse(post.content)}</div>
 
       <div className="comments-container">
