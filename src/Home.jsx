34abaa66623d63f5a6ffb301b650e4c00583e7c9
Mark A. <mark001616@gmail.com>
2024-12-08 14:56:40 -0500

    Add spinner to home posts

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Styles/index.css";
 import { format } from "date-fns";
-
+import Spinner from "./Components/Spinner.jsx";
 export default function Home({ backendUrl }) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   const postsUrl = `${backendUrl}posts`;
 
   useEffect(() => {
     fetch(postsUrl)
       .then((response) => response.json())
-      .then((data) => setPosts(data));
+      .then((data) => setPosts(data))
+      .then(() => setLoading(false));
   }, [postsUrl]);
   return (
     <>
@@ -19,18 +21,32 @@ export default function Home({ backendUrl }) {
         <h1>
           <i>lutheria</i>
         </h1>
-        <ul className="posts-list-container">
-          {posts.map((post) => (
-            <li key={post.id} className="posts-list-item">
-              <span className="posts-list-item-date">
-                {format(post.date_published, "MM-dd-yyyy")}
-              </span>
-              <Link to={"/posts/" + post.id} className="posts-list-item-link">
-                {post.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+
+        {isLoading ? (
+          <Spinner
+            spinnerColor={"#4f5d75"}
+            animationSpeed={0.9}
+            msg={"Loading Posts..."}
+          />
+        ) : (
+          <>
+            <ul className="posts-list-container">
+              {posts.map((post) => (
+                <li key={post.id} className="posts-list-item">
+                  <span className="posts-list-item-date">
+                    {format(post.date_published, "MM-dd-yyyy")}
+                  </span>
+                  <Link
+                    to={"/posts/" + post.id}
+                    className="posts-list-item-link"
+                  >
+                    {post.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </>
   );
